61581f7219f00bbda86cc687cfd3f38dce7a5e3a
Jonah <duckwxrth@gmail.com>
2026-01-26 16:07:06 -0700

    move location section and comment out equipment showcase

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,24 @@
-import Hero from '@/components/sections/Hero';
-import ServicesGrid from '@/components/sections/ServicesGrid';
-import EquipmentShowcase from '@/components/sections/EquipmentShowcase';
-import TestimonialsCarousel from '@/components/sections/TestimonialsCarousel';
-import CTABanner from '@/components/sections/CTABanner';
-import LocationSection from '@/components/sections/LocationSection';
-import PartnersLogos from '@/components/sections/PartnersLogos';
+import Hero from "@/components/sections/Hero";
+import ServicesGrid from "@/components/sections/ServicesGrid";
+// import EquipmentShowcase from '@/components/sections/EquipmentShowcase';
+import TestimonialsCarousel from "@/components/sections/TestimonialsCarousel";
+import CTABanner from "@/components/sections/CTABanner";
+import LocationSection from "@/components/sections/LocationSection";
+import PartnersLogos from "@/components/sections/PartnersLogos";
 import StructuredData, {
   localBusinessSchema,
-} from '@/components/seo/StructuredData';
+} from "@/components/seo/StructuredData";
 
 export default function Home() {
   return (
     <>
       <StructuredData data={localBusinessSchema} />
       <Hero />
+      <LocationSection />
       <ServicesGrid />
-      <EquipmentShowcase />
+      {/* <EquipmentShowcase /> */}
       <TestimonialsCarousel />
       <CTABanner />
-      <LocationSection />
       <PartnersLogos />
     </>
   );
